f541c55bceff8e42d5c053bb21284a12398a36e4
Ouzw <ouzhiwei@jointcharging.com>
2024-11-27 09:49:21 +0800

    test: add cache.ProtoBufCacheTest testcase.

--- test/cache_test.cc
+++ test/cache_test.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 
+#include "foo.pb.h"
 #include "../cache.h"
 #include "spdlog/spdlog.h"
 
@@ -54,4 +55,55 @@ TEST(Cache, BasicTest) {
     auto c3 = cachePool.Peek(&ok);
     EXPECT_EQ(ok, true);
     EXPECT_EQ(c3.name, "h"); 
+}
+
+TEST(Cache, ProtoBufCacheTest) {
+    foo::User foo1;
+    foo1.set_id(1);
+    foo1.set_name("foo1");
+
+    foo::User foo2;
+    foo2.set_id(2);
+    foo2.set_name("foo2");
+
+    foo::User foo3;
+    foo3.set_id(3);
+    foo3.set_name("foo3");
+
+
+    cache::Cache<foo::User, 3> UserCache;
+
+    UserCache.Update(foo1);
+    bool exist = false;
+
+    auto bar1 = UserCache.Peek(&exist);
+    EXPECT_EQ(exist, true);
+    EXPECT_EQ(bar1.id(), 1);
+    EXPECT_EQ(bar1.name(), "foo1");
+
+    UserCache.Update(foo2);
+    auto bar2 = UserCache.Peek(&exist);
+    EXPECT_EQ(exist, true);
+    EXPECT_EQ(bar2.id(), 2);
+    EXPECT_EQ(bar2.name(), "foo2");
+
+    UserCache.Update(foo3);
+    auto bar3 = UserCache.Peek(&exist);
+    EXPECT_EQ(exist, true);
+    EXPECT_EQ(bar3.id(), 3);
+    EXPECT_EQ(bar3.name(), "foo3");
+
+    // overwrite
+    UserCache.Update(foo1);
+    auto bar4 = UserCache.Peek(&exist);
+    EXPECT_EQ(exist, true);
+    EXPECT_EQ(bar4.id(), 1);
+    EXPECT_EQ(bar4.name(), "foo1");
+
+    // update
+    UserCache.Update(foo2);
+    auto bar5 = UserCache.Peek(&exist);
+    EXPECT_EQ(exist, true);
+    EXPECT_EQ(bar5.id(), 2);
+    EXPECT_EQ(bar5.name(), "foo2");
 }
